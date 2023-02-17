43909f6b2a51d79ab5e97545b2aeffd88b55d441
FaruqBalogun <faruqbalogun10@gmail.com>
2023-02-17 15:26:38 -0800

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints alphabet in lowercase except q and e
+ * Return: Always 0
+ */
+int main(void)
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++ '\n')
+	{
+		putchar (la);
+	}
+	for (lc != 'q' && lc != 'e''\n')
+	{
+		putchar (la);
+	}
+	return (0);
+}
