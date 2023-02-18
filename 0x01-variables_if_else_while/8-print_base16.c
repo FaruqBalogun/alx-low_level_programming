34ed29c95b894486ab940ad84b3fda33e0e8eeac
FaruqBalogun <faruqbalogun10@gmail.com>
2023-02-17 19:28:11 -0800

    Task 7

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,17 +5,17 @@
  */
 int main(void)
 {
-	int num;
-	char lc;
 
-	for (num = 0; num <= 10, num++)
-	{
+	int num;
+	int lc;
+
+	for (num = 0; num <= 10; num++)
 		putchar((num % 10) + '0');
-	}
-	for  (lc = 'a'; lc <= 'f'; lc++)
-	{
+
+	for (lc = 'a' lc <= 'f'; lc++)
 		putchar(lc);
-	}
+
 	putchar('\n');
+
 	return (0);
 }
