1a8dcbcafffdd78b742fa62b98e89f9867f58fb6
FaruqBalogun <faruqbalogun10@gmail.com>
2023-02-18 20:29:22 -0800

    Task 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * 
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 99; i++)
+	{
+		putchar('0' + i / 10);
+		putchar('0' + i % 10);
+		putchar(' ');
+	}
+
+	for (j = i + 1; j <= 99; j++);
+	{
+		putchar('0' + j / 10);
+		putchar('0' + j % 10);
+
+	}
+
+	if (i == 98 && j == 99
+		break;
+	{
+		putchar(',');
+		putchar(' ');	
+	}
+	putchar('\n');
+	return (0);
+}
