2998e25c4b3d4e66b4b029287992697a8161827e
FaruqBalogun <faruqbalogun10@gmail.com>
2023-02-24 14:48:14 -0800

    Task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * more_numbers - Print 10 times and numbers since 0 up to 24
- *
+ * more_numbers - Print 10 times and numbers since 0 up to 14
  * Return: 10 times of the numbers since 0 up to 14
  */
 
