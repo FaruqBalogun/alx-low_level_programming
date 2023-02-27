6e976648c4f4116e3b60de4dd91955e27d511cf0
FaruqBalogun <faruqbalogun10@gmail.com>
2023-02-27 06:33:38 -0800

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdout>
+
+/**
+ * print_rev - Prints a string in reverse
+ *@s: The string to be printed
+ * Return: void
+ */
+
+void print_rev(char *s)
+
+{
+
+	int c = 0;
+
+	while (s[c] != '\0')
+	{
+	c++;
+	}
+
+	for (c -= 1; c >= 0; c--)
+	{
+	_putchar(s[c]);
+	}
+	_putchar('\n');
+
+}
