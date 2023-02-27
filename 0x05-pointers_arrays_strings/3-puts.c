02460d341139d0674568e0a270afd9e9866648b2
FaruqBalogun <faruqbalogun10@gmail.com>
2023-02-27 07:36:55 -0800

    Task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,15 +3,17 @@
 /**
  * _puts -  Prints a string
  * @str: The string to print
- * Return: void
+ * Return: the length of the string
  */
 
 void _puts(char *str)
-
 {
-	for (; *str != '\0'; str++)
+	int a = 0;
+
+	while (str[a] != '\0')
 	{
-	_putchar(*str);
+		_putchar(str[a]);
+		a++;
 	}
 	_putchar('\n');
 }
