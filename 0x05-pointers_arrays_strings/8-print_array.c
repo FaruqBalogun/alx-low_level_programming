fbca2c79603e86abeb842570d36235117394b7ad
FaruqBalogun <faruqbalogun10@gmail.com>
2023-02-27 13:55:10 -0800

    Task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_array - Prints n elements of an array
+ * @a: array
+ * @n: number of values to be printed
+ */
+void print_array(int *a, int n)
+{
+	int i;
+	int n = 5;
+
+	for (i = 0; i <= 5; i++)
+	{
+	printf("%d", a[i]);
+	printf(", ");
+	}
+}
