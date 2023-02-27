e6de8af524b46e78ccd513d769c65d936d0d8877
FaruqBalogun <faruqbalogun10@gmail.com>
2023-02-27 02:26:38 -0800

    Task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * swap_int - Swaps the value of two integers
+ *
+ * @a: An integer input pointer
+ * @b: An integer input pointer
+ *
+ * Return: nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
