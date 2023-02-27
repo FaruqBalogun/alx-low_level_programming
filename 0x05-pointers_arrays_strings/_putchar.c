38be0e0c02bf6ef83928ad5750f6fccf67e1708a
FaruqBalogun <faruqbalogun10@gmail.com>
2023-02-27 13:26:50 -0800

    Task 6

--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -2,14 +2,14 @@
 
 /**
  * _putchar - writes the character c to stdout
- * @c: The character to print
+ * @str: The character to print
  *
  * Return: on succes 1.
  * on error, -1 is returned, and error is set appropriately.
  */
 
-int _putchar(char c)
+int _putchar(char *str)
 
 {
-	return (write(1, &c, 1));
+	return (write(1, &*str, 1));
 }
