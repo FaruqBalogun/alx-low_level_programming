abf4cd25723d4d2fa12955e2c397b9cf893cc12b
FaruqBalogun <faruqbalogun10@gmail.com>
2023-02-27 03:21:29 -0800

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * _strlen - Stores the length of a string
+ *
+ * @s: The length of a string
+ *
+ * Return: nothing
+ */
+
+int _strlen(char *s)
+{
+	*s = strlen(str);
+}
+
