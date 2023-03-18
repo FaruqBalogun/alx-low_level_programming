13ea0a9e82e339acff375296777d47ce2abd76c7
FaruqBalogun <faruqbalogun10@gmail.com>
2023-03-17 17:51:10 -0700

    3-array_range.c

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * *string_nconcat -  a function that concatenates two strings.
+ * @s1: String 1
+ * @s2: String 2
+ * @n: The number of bytes from s2 to cocatenate to s1i
+ * Return: Always 0.
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+
+}
