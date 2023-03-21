d996acbd4c0bd9ece61cc2c60879e4ea2ce5e15f
FaruqBalogun <faruqbalogun10@gmail.com>
2023-03-20 21:45:44 -0700

    typedef

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib>
+
+/**
+  * free_dog - This frees the dog
+  * @d: Dog pointer
+  */
+void free_dog(dog_t *d)
+{
+	d->name = malloc(...);
+	d->owner = malloc(..);
+	dog_t *p = malloc(..);
+
+	free(d->name)
+	free(d->owner)
+	free(d);
+	d = null
+}
