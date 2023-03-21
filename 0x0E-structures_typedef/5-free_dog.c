3fcfc04f7dfae2b60ad469b6fa44866facdca3d8
FaruqBalogun <faruqbalogun10@gmail.com>
2023-03-21 16:36:01 -0700

    4-new_dog.c and 5-free_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,5 +1,5 @@
 #include "dog.h"
-#include <stdlib>
+#include <stdlib.h>
 
 /**
   * free_dog - This frees the dog
@@ -7,12 +7,10 @@
   */
 void free_dog(dog_t *d)
 {
-	d->name = malloc(...);
-	d->owner = malloc(..);
-	dog_t *p = malloc(..);
-
-	free(d->name)
-	free(d->owner)
-	free(d);
-	d = null
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
 }
